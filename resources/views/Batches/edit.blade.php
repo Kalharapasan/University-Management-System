85fc759f224db54fdcdc7e827258d9c314440307
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-08-17 20:07:25 +0530

    complete course parat in system

--- resources/views/Batches/edit.blade.php
+++ resources/views/Batches/edit.blade.php
@@ -1,33 +1,41 @@
-<form action="{{ route('batch/'. $batch->id) }}" method="POST">
-    @csrf
-    @method('PUT')
-    <div class="row">
-        <div class="col-md-6">
-            <label>Batch Name</label>
-            <input class="form-control" name="name" value="{{ $batch->name ?? '' }}" required>
-        </div>
-        <div class="col-md-6">
-            <label>Course</label>
-            <select class="form-control" name="course_id" required>
-                <option value="">-- Select Course --</option>
-                @foreach($courses as $course)
-                    <option value="{{ $course->id }}" {{ (isset($batch) && $batch->course_id == $course->id) ? 'selected' : '' }}>
-                        {{ $course->name }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-    </div>
+@extends('layout')
+@section('content')
 
-    <div class="row mt-3">
-        <div class="col-md-6">
-            <label>Start Date</label>
-            <input type="date" class="form-control" name="start_date" value="{{ $batch->start_date ?? '' }}" required>
+<div class="container">
+    <div class="card mt-5 shadow">
+        <div class="card-header bg-primary text-white">
+            <h3 class="mb-0 text-center">Update Batch Details</h3>
+        </div>
+        <div class="card-body">
+            <form action="{{ url('batch/'.$batch->id) }}" method="POST">
+                {!! csrf_field() !!}
+                @method('PUT')
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <label class="form-label">Batch Name</label>
+                        <input class="form-control" name="name" value="{{ $batch->name ?? '' }}" required>
+                    </div>
+                    <div class="col-md-6">
+                        <label class="form-label">Course ID</label>
+                        <input type="text" class="form-control" name="course_id" value="{{ $batch->course_id ?? '' }}" required>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <label class="form-label">Start Date</label>
+                        <input type="date" class="form-control" name="start_date" value="{{ $batch->start_date ?? '' }}" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-end">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                        <a href="{{ url('batch') }}" class="btn btn-secondary ms-2">Cancel</a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-    <div class="row mt-3">
-        <div class="col-md-12">
-            <button type="submit" class="btn btn-primary">Update Batch</button>
-        </div>
-    </div>
-</form>
+</div>
+
+@endsection
+
